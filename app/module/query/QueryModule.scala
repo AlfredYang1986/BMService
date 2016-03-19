cb4f025a49e45e413c76a3fc5b55ae710b94fab7
AlfredYang1986 <yangyuanpig@gmail.com>
2016-03-19 23:27:48 +0800

    get role tag crash bug

--- app/module/query/QueryModule.scala
+++ app/module/query/QueryModule.scala
@@ -50,8 +50,7 @@ object QueryModule {
 	  	  val isPush = PostModule.isPush(user_id, post_id)
 	  	  tmp += "isPush" -> toJson(isPush)
 	  	  
-	  	  val role_tag = ProfileModule.queryUserProfile(user_id).get("role_tag").get
-	  	  tmp += "role_tag" -> toJson(role_tag)
+	  	  tmp += "role_tag" -> toJson((ProfileModule.queryUserProfile(tmp.get("owner_id").get.asOpt[String].get)).get("role_tag").get.asOpt[String].map(rt => rt).getOrElse(""))
 	  	  
 		  	xls = xls :+ toJson(tmp)
 		}
@@ -106,8 +105,8 @@ object QueryModule {
 
 	  	  val isPush = PostModule.isPush(user_id, post_id)
 	  	  tmp += "isPush" -> toJson(isPush)
-	  	 
-	  	  tmp += "role_tag" -> toJson((ProfileModule.queryUserProfile(tmp.get("owner_id").get.asOpt[String].get)).get("role_tag").get.asOpt[String].get)
+	  	
+	  	  tmp += "role_tag" -> toJson((ProfileModule.queryUserProfile(tmp.get("owner_id").get.asOpt[String].get)).get("role_tag").get.asOpt[String].map(rt => rt).getOrElse(""))
 
 		  	xls = xls :+ toJson(tmp)
 		}	
