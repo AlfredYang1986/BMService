9340f56a2b8da7bf602f87d391568e932217d432
AlfredYang1986 <yangyuanpig@gmail.com>
2016-11-11 17:19:55 +0800

    Missing files

--- app/controllers/v2/AuthController.scala
+++ app/controllers/v2/AuthController.scala
@@ -7,11 +7,24 @@ import controllers.common.requestArgsQuery._
 
 import dongdamessages.MessageRoutes
 import module.auth.AuthMessage.msg_AuthPhoneCode
+import module.auth.AuthMessage.msg_AuthThird
+import module.auth.AuthMessage.msg_AuthSignOut
 import module.phonecode.PhoneCodeMessages.msg_CheckSMSCode
 import module.profile.v2.ProfileMessages.msg_UpdateProfile
 import module.emxmpp.EMMessages.msg_RegisterEMUser
+import pattern.ResultMessage.msg_CommonResultMessage
 
 object AuthController extends Controller {
-	def authWithPhoneCode = Action (request => requestArgsV2(request)(jv => MessageRoutes(msg_CheckSMSCode(jv) :: msg_AuthPhoneCode(jv) :: msg_UpdateProfile(jv) :: msg_RegisterEMUser(jv) :: Nil, None)))
-	def authWithThird = Action (request => requestArgsV2(request)(jv => MessageRoutes(msg_CheckSMSCode(jv) :: msg_AuthPhoneCode(jv) :: msg_UpdateProfile(jv) :: msg_RegisterEMUser(jv) :: Nil, None)))
+	def authWithPhoneCode = Action (request => requestArgsV2(request) { jv => 
+			import pattern.ResultMessage.common_result
+			MessageRoutes(msg_CheckSMSCode(jv) :: msg_AuthPhoneCode(jv) :: msg_UpdateProfile(jv) :: msg_RegisterEMUser(jv) :: msg_CommonResultMessage() :: Nil, None)
+		})
+	def authWithThird = Action (request => requestArgsV2(request) { jv => 
+			import pattern.ResultMessage.common_result
+			MessageRoutes(msg_CheckSMSCode(jv) :: msg_AuthThird(jv) :: msg_UpdateProfile(jv) :: msg_RegisterEMUser(jv) :: msg_CommonResultMessage() :: Nil, None)
+		})
+	def authSignOut = Action (request => requestArgsV2(request) { jv => 
+			import pattern.ResultMessage.common_result
+			MessageRoutes(msg_AuthSignOut(jv) :: msg_CommonResultMessage() :: Nil, None)
+		})
 }
--- app/controllers/v2/ProfileController.scala
+++ app/controllers/v2/ProfileController.scala
@@ -7,8 +7,15 @@ import controllers.common.requestArgsQuery._
 
 import dongdamessages.MessageRoutes
 import module.profile.v2.ProfileMessages._
+import pattern.ResultMessage.msg_CommonResultMessage
 
 object ProfileController extends Controller {
-	def updateUserProfile = Action (request => requestArgsV2(request)(jv => MessageRoutes(msg_UpdateProfile(jv) :: Nil, None)))
-	def queryUserProfile = Action (request => requestArgsV2(request)(jv => MessageRoutes(msg_QueryProfile(jv) :: Nil, None)))
+	def updateUserProfile = Action (request => requestArgsV2(request) { jv => 
+			import pattern.ResultMessage.common_result
+			MessageRoutes(msg_UpdateProfile(jv) :: msg_CommonResultMessage() ::Nil, None)
+		})
+	def queryUserProfile = Action (request => requestArgsV2(request) { jv => 
+			import pattern.ResultMessage.common_result
+			MessageRoutes(msg_QueryProfile(jv) :: msg_CommonResultMessage() :: Nil, None)
+		})
 }
