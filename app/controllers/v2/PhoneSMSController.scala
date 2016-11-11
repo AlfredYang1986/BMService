5fa446cb2142ffc7b9720fa3126793b6d55feecf
AlfredYang1986 <yangyuanpig@gmail.com>
2016-11-11 17:00:09 +0800

    missing files

--- app/controllers/v2/PhoneSMSController.scala
+++ app/controllers/v2/PhoneSMSController.scala
@@ -7,8 +7,15 @@ import controllers.common.requestArgsQuery._
 
 import dongdamessages.MessageRoutes
 import module.phonecode.PhoneCodeMessages._
+import pattern.ResultMessage.msg_CommonResultMessage
 
 object PhoneSMSController extends Controller {
-	def sendSMSCode = Action (request => requestArgsV2(request)(jv => MessageRoutes(msg_SendSMSCode(jv) :: Nil, None)))
-	def checkSMSCode = Action (request => requestArgsV2(request)(jv => MessageRoutes(msg_CheckSMSCode(jv) :: Nil, None)))
+	def sendSMSCode = Action (request => requestArgsV2(request) { jv => 
+			import pattern.ResultMessage.common_result
+			MessageRoutes(msg_SendSMSCode(jv) :: msg_CommonResultMessage() :: Nil, None)
+		})
+	def checkSMSCode = Action (request => requestArgsV2(request) { jv => 
+			import pattern.ResultMessage.common_result
+			MessageRoutes(msg_CheckSMSCode(jv) :: msg_CommonResultMessage() :: Nil, None)
+		})
 }
